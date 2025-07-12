07e11745e0f9bde83897d1b57a006ba91af3f7ba
kiki2010 <chiaraangelinacatalini@gmail.com>
2025-07-12 20:25:40 -0300

    started working on displaying projects

---
+++ script/script.js
@@ -0,0 +1,104 @@
+const projectData = {
+    apps: [
+        {
+            title: "FlameFighters",
+            description: "Is an app to help prevent wildfires by knowing local risk, easy to use and install.",
+            image: "",
+            award: "TG 2024 Finalist | TG 2024 Social Impact",
+            download: "https://drive.google.com/file/d/18ZRr60LK3G1Hk6mRzfUu4LIiLH605Es7/view?usp=sharing",
+        },
+        {
+            title: "RiverStream",
+            description: "From 1970 to 2019, floods were the most frequent weather, climate, and water-related disasters, accounting for 44% of all disasters globally. However, flood prediction apps are available only in some countries, leaving many regions unprepared. To address this, we developed RiverStream, an app that can be used worldwide. It allows users to receive and contribute flood data, while AI predicts floods in useful time and notifies users when risk increases. A simulation feature visualizes flood scenarios, raising awareness and preparedness. RiverStream builds a collaborative network to strengthen global disaster response, promoting action, prevention, and education.",
+            image: "",
+            award: "TG 2025 Regional Honoree Latin America | TG 2025 Special Award ClimatePrize | AI Favorite Coolest Projects 2025",
+            download: "https://drive.google.com/file/d/1tl-qEZ-nsGtlDHISIv4JyX_9gIaMRI_h/view?pli=1",
+        },
+    ],
+    robots: [
+        {
+            title: "Wall-e",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "R2-D2",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "KikiBot",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        }
+    ],
+    extra: [
+        {
+            title: "Mes de las Infancias 2023",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "PracTICarlos 2023",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "RandomPlay 2024",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "Encuentro de Innovadores 2024",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "Technovation Girls 2024",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "RandomPlay 2025",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "Technovation Girls 2025",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+        {
+            title: "Coolest Projects 2025",
+            description: "",
+            image: "",
+            award: "",
+            download: "",
+        },
+    ]
+};
+
+function showCategory(category) {
+    const container = document.getElementById("projects-display");
+    container.innerHTML = "";
+}
