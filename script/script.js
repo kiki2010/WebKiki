48b1de366a66d6e5747a024c35e084f72bb1a0d4
kiki2010 <chiaraangelinacatalini@gmail.com>
2025-07-13 20:02:25 -0300

    continue adding sections on category

--- script/script.js
+++ script/script.js
@@ -1,102 +1,263 @@
 const projectData = {
-    apps: [
-        {
-            title: "FlameFighters",
-            description: "Is an app to help prevent wildfires by knowing local risk, easy to use and install.",
-            image: "",
-            award: "TG 2024 Finalist | TG 2024 Social Impact",
-            download: "https://drive.google.com/file/d/18ZRr60LK3G1Hk6mRzfUu4LIiLH605Es7/view?usp=sharing",
-        },
-        {
-            title: "RiverStream",
-            description: "From 1970 to 2019, floods were the most frequent weather, climate, and water-related disasters, accounting for 44% of all disasters globally. However, flood prediction apps are available only in some countries, leaving many regions unprepared. To address this, we developed RiverStream, an app that can be used worldwide. It allows users to receive and contribute flood data, while AI predicts floods in useful time and notifies users when risk increases. A simulation feature visualizes flood scenarios, raising awareness and preparedness. RiverStream builds a collaborative network to strengthen global disaster response, promoting action, prevention, and education.",
-            image: "",
-            award: "TG 2025 Regional Honoree Latin America | TG 2025 Special Award ClimatePrize | AI Favorite Coolest Projects 2025",
-            download: "https://drive.google.com/file/d/1tl-qEZ-nsGtlDHISIv4JyX_9gIaMRI_h/view?pli=1",
-        },
-    ],
-    robots: [
-        {
-            title: "Wall-e",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "R2-D2",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "KikiBot",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        }
-    ],
-    extra: [
-        {
-            title: "Mes de las Infancias 2023",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "PracTICarlos 2023",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "RandomPlay 2024",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "Encuentro de Innovadores 2024",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "Technovation Girls 2024",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "RandomPlay 2025",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "Technovation Girls 2025",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-        {
-            title: "Coolest Projects 2025",
-            description: "",
-            image: "",
-            award: "",
-            download: "",
-        },
-    ]
-};
+  apps:[
+    {
+      title: "FlameFighters",
+      description: "Is an app to help prevent wildfires by knowing local risk, easy to use and install.",
+      image: "img/apps/FlameFighters.png",
+      award: "TG 2024 Finalist | TG 2024 Social Impact",
+      download: "https://drive.google.com/file/d/18ZRr60LK3G1Hk6mRzfUu4LIiLH605Es7/view?usp=sharing",
+    },
+    {
+      title: "RiverStream",
+      description: "We developed RiverStream, an app that can be used worldwide. It allows users to receive and contribute flood data, while AI predicts floods in useful time and notifies users when risk increases. A simulation feature visualizes flood scenarios, raising awareness and preparedness.",
+      image: "img/apps/riverstream.png",
+      award: "TG 2025 Regional Honoree Latin America | TG 2025 Special Award ClimatePrize | AI Favorite Coolest Projects 2025",
+      download: "https://drive.google.com/file/d/1tl-qEZ-nsGtlDHISIv4JyX_9gIaMRI_h/view?pli=1",
+    }
+  ],
+  robots: [
+    {
+      title: "Wall-e",
+      description: "Inspired by the Disney movie, an educational project with an ESP8266 controlled via Wi-Fi.",
+      image: "img/robots/wall-e.jpeg",
+      award: " ",
+      download: " ",
+    },
+    {
+      title: "R2D2",
+      description: "Line follower inspired by the robot from the Star Wars saga.",
+      image: "img/robots/r2d2.jpg",
+      award: " ",
+      download: " ",
+    },
+    {
+      title: "KikiBot",
+      description: "Small UFO-shaped battle robot.",
+      image: "img/robots/sumo.jpg",
+      award: " ",
+      download: " ",
+    }
+  ],
+  extra: [
+    {
+      title: "2016: participation in the ADOPTO project.",
+      description: "First contact with citizen science.",
+      image: "",
+      award: " ",
+      download: " ",
+    },
+    {
+      title: "2018: participation in the MATTEO project.",
+      description: "Contributing with recorded rainfall data.",
+      image: "",
+      award: " ",
+      download: " ",
+    },
+    {
+      title: "2020: Book Review: 'Fieldwork: Ayla's Adventures on Earth'",
+      description: "“...I read it twice yesterday. I liked it because it reinforced all the information I learned last year, like the Mar Chiquita lagoon and the water cycle. It was good because it's easy to understand, and when it talks about evaporation, I like that it mentions plant transpiration...”.",
+      image: "",
+      award: "",
+      download: "https://drive.google.com/file/d/1VK_yglDDCMZPrz-4iQBXhRKt62SiboAO/view"
+    },
+    {
+      title: "2021: Best School average and national flag bearer",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2022: First TV interview",
+      description: "Because of the international day of the women and girls in science.",
+      image: "",
+      award: "",
+      download: "https://youtu.be/NKntUT4E0g0?si=7oN4s4yLM3181AhW",
+    },
+    {
+      title: "2022: Creation of the Social Media of 'Guardianes del Lago'",
+      description: "Where the care of the San Roque dam and our environment was promoted",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2022: Kiki! Youtube Channel Creation",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "Get B1 english level",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2023:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2023:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2023:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2023: ",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2023:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024: ",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2024:",
+      description: "",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2025: RandomPlay",
+      description: "Showing the new app RiverStream.",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2025: Technovation Girls",
+      description: "Submitting the RiverStream application.",
+      image: "",
+      award: "",
+      download: "",
+    },
+    {
+      title: "2025: Coolest Projects",
+      description: "Submitting the RiverStream application.",
+      image: "",
+      award: "",
+      download: "",
+    },
+  ]
+}
 
 let currentIndex = 0;
 let currentCategory = "";
@@ -127,7 +288,7 @@ function renderCarousel() {
       <div class="project-title">${project.title}</div>
       <div class="project-description">${project.description || "No description available."}</div>
       ${project.award ? `<div class="project-awards">🏆 ${project.award}</div>` : ""}
-      ${project.download ? `<a class="project-link" href="${project.download}" target="_blank">🔗 Download </a>` : ""}
+      ${project.download ? `<a class="project-link" href="${project.download}" target="_blank">🔗Check it! </a>` : ""}
     </div>
   `;
 
